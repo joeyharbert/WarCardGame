85916eb0766066cf92be647f3dd1247116184c24
joeyharbert <joey.harbert@gmail.com>
2019-09-07 12:56:19 -0500

    Initial Commit

---
+++ WarCardGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WarCardGame
+//
+//  Created by Joey Harbert on 9/7/19.
+//  Copyright © 2019 Joey Harbert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
